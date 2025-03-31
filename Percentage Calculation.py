11a6c8b6d282143f7f912b228e0c966b1326de16
AdhritShetty <adhritshetty2009@gmail.com>
2025-03-31 16:18:27 +0000

    Percentage Calculation

---
+++ Percentage Calculation.py
@@ -0,0 +1,13 @@
+#Finding the marks obtained by the student 
+Hindi=79
+Social=87
+Maths=83
+Science=88
+English=84
+
+#finding the total marks obtained by the student
+Sum=Hindi+Social+Maths+Science+English
+
+#Finding the percentage of th student
+perc=(Sum/500)*100
+print("the percentage of the student is:",perc)
