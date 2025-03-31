09fd4b5a43c653bb5f2b092cbbce18772ea0deff
AdhritShetty <adhritshetty2009@gmail.com>
2025-03-31 15:50:26 +0000

    Finding average

---
+++ Storing Values.py
@@ -0,0 +1,13 @@
+#Storing Vlues
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+#finding total of the trees
+sum=tree1+tree2+tree3+tree4+tree5
+
+#finding the average of the trees
+average=sum/5
+print("the average of the all the tree's are:",average)
